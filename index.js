5bcc36f6505ddc94dbe65dffd5f4c60bce0a7418
Matthew Yang <matthewyang434@gmail.com>
2023-07-31 16:56:57 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  // fetch('https://anapioficeandfire.com/api/books')
+  // .then(function (response) {
+  //   return response.json();
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
+  }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
